5b0e23b5f9a3abead44aa322a88fe401e61766bf
Brahms <bramso.brahms@gmail.com>
2026-02-06 19:06:19 +0100

    Create project Api First Enterprise

---
+++ src/main/java/be/community/Api_First_Enterprise/ApiFirstEnterpriseApplication.java
@@ -0,0 +1,13 @@
+package be.community.Api_First_Enterprise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiFirstEnterpriseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiFirstEnterpriseApplication.class, args);
+	}
+
+}
